af78011eae45dd98372149a087aaf669b06e0bee
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-09 13:00:06 -0600

    added in script for start and changed port to dynamic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const server = require('./server.js');
+const port = process.env.PORT || 4090;
 
 
-server.listen(4090, ()=>{
-    console.log('\n****Server Running on port 4090***\n')
-})
+server.listen(port, ()=>{
+    console.log(`\n****Server Running on port ${port}***\n`)
+}) 
